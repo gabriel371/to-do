7685c3340a3145717a1f6e88d6f94bfa474411f8
Gabriel Lucena <gabrielml371@gmail.com>
2020-07-24 23:41:48 -0300

    changes on home page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -16,12 +16,81 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Column(
-        children: <Widget>[],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.125,
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  decoration: BoxDecoration(
+                    color: Colors.lime[600],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.125,
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  decoration: BoxDecoration(
+                    color: Colors.lime[600],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.125,
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  decoration: BoxDecoration(
+                    color: Colors.lime[600],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+            child: Text(
+              widget.title,
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.check),
+            title: Text(
+              'Task #1',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            trailing: Icon(Icons.delete),
+          ),
+          ListTile(
+            leading: Icon(Icons.close),
+            title: Text(
+              'Task #2',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            trailing: Icon(Icons.delete),
+          ),
+          ListTile(
+            leading: Icon(Icons.check),
+            title: Text(
+              'Task #3',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            trailing: Icon(Icons.delete),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+        ],
       ),
+      backgroundColor: Colors.lime[300],
     );
   }
 }
